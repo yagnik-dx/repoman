dce5bda5aaac3d4abe898f27ffe6d477eeddaa03
yagnik <yagnik@getaltadx.com>
2026-04-17 17:56:31 +0530

    feat: list and selected commands

---
+++ cmd/list.go
@@ -0,0 +1,36 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"repoman/internal/config"
+	"repoman/internal/git"
+
+	"github.com/spf13/cobra"
+)
+
+var listCmd = &cobra.Command{
+	Use:     "list",
+	Short:   "List all git repos found under basePath",
+	Example: "  repoman list",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		cfg, err := config.Load()
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+		repos, err := git.ScanRepos(cfg.BasePath)
+		if err != nil {
+			return fmt.Errorf("scan repos: %w", err)
+		}
+		for _, r := range repos {
+			fmt.Println(r)
+		}
+		return nil
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(listCmd)
+}
---
+++ cmd/selected.go
@@ -0,0 +1,31 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"repoman/internal/config"
+
+	"github.com/spf13/cobra"
+)
+
+var selectedCmd = &cobra.Command{
+	Use:     "selected",
+	Short:   "List currently selected repos",
+	Example: "  repoman selected",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		cfg, err := config.Load()
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+		for _, r := range cfg.SelectedRepos {
+			fmt.Println(r)
+		}
+		return nil
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(selectedCmd)
+}
